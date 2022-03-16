a50dd94b7095a0585d165edc6c516d9ca97ee025
yuntae-go <dbsxo7625@naver.com>
2022-03-16 21:18:55 +0900

    add : color 변수 추가

---
+++ src/constants/color.js
@@ -0,0 +1,7 @@
+export const PRIMARY = '#3366ff';
+export const SECONDARY = '#22bd79';
+export const WHITE = '#ffffff';
+export const BLACK = '#000000';
+export const GRAY = '#cccccc';
+export const LIGHT_GRAY = '#f2f4f7';
+export const LINE_COLOR = '#c4c4c4';
