b71a071933df72505d33b974fdd74794bcfd22df
yuntaengtaeng <k76253218@gmail.com>
2022-03-24 02:52:29 +0900

    add : styled-reset 추가

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
 import AppInner from './AppInner';
+import { Reset } from 'styled-reset';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Reset />
       <AppInner></AppInner>
     </Provider>
   );
