afd5fffcd2f0a03ca79fbe469ce605546a02c9d3
yuntaengtaeng <k76253218@gmail.com>
2022-03-29 20:15:12 +0900

    feat : Auth Login, Join 페이지 개발
    - API 연결

--- src/pages/Auth/Join.js
+++ src/pages/Auth/Join.js
@@ -1,7 +1,273 @@
-import React from 'react';
+import React, { useCallback, useState, useRef } from 'react';
+import styled from 'styled-components';
+import Input from '../../components/Ui/Input';
+import Button, { BUTTON_THEME } from '../../components/Ui/Button';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import uiSlce from '../../slices/ui';
+import { useDispatch } from 'react-redux';
+
+const Form = styled.form`
+  margin: auto;
+  width: 60%;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const RaidoRow = styled(Row)`
+  margin: 1rem 0px;
+`;
+
+const Certification = styled(Button)`
+  min-width: 200px;
+
+  @media all and (max-width: 479px) {
+    min-width: 100px;
+  }
+`;
+
+const SubmitButton = styled(Button)`
+  margin: 2em auto;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin: 1rem 0px;
+  text-align: left;
+`;
 
 const Join = () => {
-  return <div>Join</div>;
+  const [isSendEmail, setIsSendEmail] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [role, setRole] = useState('student');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const inputRef = useRef([]);
+
+  const requestEmailFromServer = useCallback(async () => {
+    const id = inputRef.current.id.value.trim();
+
+    const emailRegex =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+    if (!emailRegex.test(id)) {
+      return;
+    }
+
+    dispatch(uiSlce.actions.showLoading());
+
+    try {
+      const resposne = await axios.post('api/auth/code/email', {
+        id,
+      });
+
+      const {
+        data: { isSend },
+      } = resposne;
+
+      setIsSendEmail(isSend);
+    } catch (error) {
+      setIsSendEmail(false);
+      if (error.response) {
+        alert(error.response.data.message);
+        return;
+      }
+
+      alert(error.message);
+    } finally {
+      dispatch(uiSlce.actions.hideLoading());
+    }
+  }, [dispatch]);
+
+  const requestAuthEmailFromServer = useCallback(async () => {
+    const id = inputRef.current.id.value.trim();
+    const code = inputRef.current.code.value.trim();
+
+    if (!id || !code) {
+      return;
+    }
+
+    dispatch(uiSlce.actions.showLoading());
+
+    try {
+      const resposne = await axios.post('api/auth/email', {
+        id,
+        code,
+      });
+
+      const {
+        data: { isAuth },
+      } = resposne;
+
+      setIsValidEmail(isAuth);
+    } catch (error) {
+      setIsValidEmail(false);
+      if (error.response) {
+        alert(error.response.data.message);
+        return;
+      }
+
+      alert(error.message);
+    } finally {
+      dispatch(uiSlce.actions.hideLoading());
+    }
+  }, [dispatch]);
+
+  const jobChangeHandler = (event) => {
+    setRole(event.target.value);
+  };
+
+  const onSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+
+      if (!isSendEmail || !isValidEmail) {
+        return;
+      }
+
+      const inputs = inputRef.current;
+      const { body, isFullFill } = Object.entries(inputs).reduce(
+        (data, [key, elem]) => {
+          if (!!elem) {
+            const value = elem.value.trim();
+
+            if (!value) {
+              data.isFullFill = false;
+            }
+
+            data.body[key] = value;
+          }
+
+          return data;
+        },
+        { body: {}, isFullFill: true }
+      );
+
+      if (!isFullFill) {
+        return;
+      }
+
+      Object.assign(body, { isStudent: role === 'student' });
+
+      dispatch(uiSlce.actions.showLoading());
+
+      try {
+        const resposne = await axios.post('api/auth/join', {
+          ...body,
+        });
+
+        const {
+          data: { isJoin },
+        } = resposne;
+
+        if (isJoin) {
+          navigate('/');
+        }
+      } catch (error) {
+        if (error.response) {
+          alert(error.response.data.message);
+          return;
+        }
+
+        alert(error.message);
+      } finally {
+        dispatch(uiSlce.actions.hideLoading());
+      }
+    },
+    [dispatch, isSendEmail, isValidEmail, navigate, role]
+  );
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Label>아이디(이메일)</Label>
+      <Row>
+        <Input
+          type="text"
+          ref={(el) => (inputRef.current['id'] = el)}
+          disabled={isSendEmail}
+        />
+        <Certification
+          type="button"
+          theme={BUTTON_THEME.PRIMARY}
+          onClick={requestEmailFromServer}
+          disabled={isSendEmail}
+        >
+          이메일 인증
+        </Certification>
+      </Row>
+      <Label>이메일 인증 코드</Label>
+      <Row>
+        <Input
+          type="text"
+          ref={(el) => (inputRef.current['code'] = el)}
+          disabled={isValidEmail}
+        />
+        <Certification
+          type="button"
+          theme={BUTTON_THEME.PRIMARY}
+          onClick={requestAuthEmailFromServer}
+          disabled={isValidEmail}
+        >
+          인증하기
+        </Certification>
+      </Row>
+      <Label>닉네임</Label>
+      <Input type="text" ref={(el) => (inputRef.current['nickname'] = el)} />
+      <Label>비밀번호</Label>
+      <Input
+        type="password"
+        ref={(el) => (inputRef.current['password'] = el)}
+      />
+      <Label>비밀번호 확인</Label>
+      <Input
+        type="password"
+        ref={(el) => (inputRef.current['verifyPassword'] = el)}
+      />
+      <RaidoRow>
+        <label htmlFor="student">
+          학생
+          <input
+            id="student"
+            value="student"
+            name="role"
+            type="radio"
+            checked={role === 'student'}
+            onChange={jobChangeHandler}
+          />
+        </label>
+        <label htmlFor="basic">
+          일반
+          <input
+            id="basic"
+            value="basic"
+            name="role"
+            type="radio"
+            checked={role === 'basic'}
+            onChange={jobChangeHandler}
+          />
+        </label>
+      </RaidoRow>
+      {role === 'student' && (
+        <>
+          <Label>학번</Label>
+          <Input type="text" ref={(el) => (inputRef.current['uniId'] = el)} />
+          <Label>이름</Label>
+          <Input type="text" ref={(el) => (inputRef.current['name'] = el)} />
+          <Label>생년월일(8자리)</Label>
+          <Input
+            type="text"
+            ref={(el) => (inputRef.current['birthday'] = el)}
+          />
+        </>
+      )}
+      <SubmitButton type="submit" theme={BUTTON_THEME.PRIMARY}>
+        회원가입
+      </SubmitButton>
+    </Form>
+  );
 };
 
 export default Join;
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,7 +1,144 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import styled from 'styled-components';
+import Button, { BUTTON_THEME } from '../../components/Ui/Button';
+import Input from '../../components/Ui/Input';
+import { GRAY } from '../../constants/color';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import userSlice from '../../slices/user';
+import uiSlce from '../../slices/ui';
+
+const Container = styled.div`
+  width: 80%;
+  margin: auto;
+`;
+
+const Introduce = styled.div`
+  margin-top: 2rem;
+  text-align: center;
+  margin-bottom: 4rem;
+`;
+
+const Title = styled.h2`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const SubTitle = styled.p`
+  font-size: 1.4rem;
+  color: ${GRAY};
+`;
+
+const StyledForm = styled.form`
+  margin: auto;
+  text-align: center;
+  width: 40%;
+
+  @media all and (max-width: 479px) {
+    width: 100%;
+  }
+`;
+
+const Label = styled.label`
+  display: block;
+  margin: 1rem 0px;
+  text-align: left;
+`;
+
+const StyledButton = styled(Button)`
+  margin: 2rem auto;
+`;
+
+const Join = styled.span`
+  color: ${GRAY};
+  text-decoration: underline;
+`;
 
 const Login = () => {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const idRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const moveJoin = useCallback(() => {
+    navigate('/join');
+  }, [navigate]);
+
+  const onSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+
+      const id = idRef.current.value;
+      const password = passwordRef.current.value;
+
+      if (!id || !password) {
+        return;
+      }
+
+      dispatch(uiSlce.actions.showLoading());
+
+      try {
+        const resposne = await axios.post('api/auth/login', {
+          id,
+          password,
+        });
+
+        const {
+          data: { data },
+        } = resposne;
+
+        const { refreshToken, ...rest } = data;
+        dispatch(userSlice.actions.setUser(rest));
+        localStorage.setItem('refreshToken', refreshToken);
+
+        navigate('/');
+      } catch (error) {
+        if (error.response) {
+          alert(error.response.data.message);
+          return;
+        }
+
+        alert(error.message);
+      } finally {
+        dispatch(uiSlce.actions.hideLoading());
+      }
+    },
+    [dispatch, navigate]
+  );
+
+  return (
+    <Container>
+      <Introduce>
+        <Title>
+          서일대를 위한
+          <br />
+          커리어 플랫폼, boogie on &#38; on!
+        </Title>
+        <SubTitle>
+          커리어 성장과 행복을 위한 여정
+          <br />
+          지금 boogie on &#38; on에서 시작하세요.
+        </SubTitle>
+      </Introduce>
+      <StyledForm onSubmit={onSubmit}>
+        <Label>아이디</Label>
+        <Input ref={idRef} type="text" />
+        <Label>비밀번호</Label>
+        <Input
+          ref={passwordRef}
+          type="password"
+          autocomplete="current-password"
+        />
+        <StyledButton type="submit" theme={BUTTON_THEME.PRIMARY}>
+          로그인
+        </StyledButton>
+        <Join onClick={moveJoin}>회원가입</Join>
+      </StyledForm>
+    </Container>
+  );
 };
 
 export default Login;
