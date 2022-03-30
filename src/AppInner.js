2ba6455f1b1b53533a133f1fd8e402a37c7eba0f
yuntaengtaeng <k76253218@gmail.com>
2022-03-30 20:42:21 +0900

    feat : axios로 api 요청 후 accessToken이 유효하지 않다면 토큰 재발급 후 다시 요청하도록 axios interceptor 추가
    - refreshToken이 유효하다면 자동 로그인이 되도록 기능 추가

--- src/AppInner.js
+++ src/AppInner.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
@@ -31,13 +31,117 @@ import AdminRoute from './components/Route/AdminRoute';
 
 import Loading from './components/Ui/Loading';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import uiSlce from './slices/ui';
+import userSlice from './slices/user';
 
 import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 const AppInner = () => {
   const isLoading = useSelector((state) => state.ui.isLoading);
+  const dispatch = useDispatch();
+
+  //api 요청 후 accessToekn이 유효하지 않다면 재발급 후 다시 요청, refreshToken이 유효하지 않다면 사용자에게 다시 로그인 요청
+  useEffect(() => {
+    axios.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (error) => {
+        const {
+          config,
+          response: { status },
+        } = error;
+
+        if (status === 419) {
+          if (error.response.data.code === 'expired') {
+            const type = error.response.data.type;
+
+            if (type === 'refresh') {
+              dispatch(userSlice.actions.initUser());
+              alert('다시 로그인해주세요.');
+              return;
+            } else if (type === 'access') {
+              const originalRequest = config;
+              const refreshToken = localStorage.getItem('refreshToken');
+
+              const {
+                data: { data },
+              } = await axios.post(
+                'api/token/refresh-token',
+                {},
+                {
+                  headers: {
+                    authorization: `Boogie ${refreshToken}`,
+                  },
+                }
+              );
+
+              //새로운 토큰 저장
+              dispatch(userSlice.actions.setAccessToken(data));
+              //419 요청에 실패했던 요청 새로운 토큰으로 재요청
+              originalRequest.headers.authorization = `Boogie ${data.accessToken}`;
+
+              return axios(originalRequest);
+            }
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  }, [dispatch]);
+
+  //localStorage가 존재한다면 유효한지 판단 후 자동 로그인
+  useEffect(() => {
+    const requestVerifyRefreshToken = async () => {
+      const refreshToken = localStorage.getItem('refreshToken');
+
+      if (!refreshToken) {
+        return;
+      }
+
+      try {
+        const {
+          data: { isValid },
+        } = await axios.post(
+          'api/token/verify/refresh-token',
+          {},
+          {
+            headers: {
+              authorization: `Boogie ${refreshToken}`,
+            },
+          }
+        );
+
+        if (!isValid) {
+          throw new Error('Pass');
+        }
+
+        const {
+          data: { data },
+        } = await axios.post(
+          'api/token/refresh-token',
+          {},
+          {
+            headers: {
+              authorization: `Boogie ${refreshToken}`,
+            },
+          }
+        );
+
+        //새로운 토큰 저장
+        dispatch(userSlice.actions.setUser(data));
+      } catch (error) {
+        localStorage.removeItem('refreshToken');
+      } finally {
+        console.log('finally');
+      }
+    };
+
+    requestVerifyRefreshToken();
+  }, [dispatch]);
 
   return (
     <BrowserRouter>
