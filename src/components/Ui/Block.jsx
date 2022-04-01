13f81569d71452645434ba33418e900401e70153
yuntaengtaeng <k76253218@gmail.com>
2022-04-01 22:53:16 +0900

    feat : Block 컴포넌트 추가

---
+++ src/components/Ui/Block.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBlock = styled.header`
+  width: 100%;
+  height: 56px;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+`;
+
+const Block = ({ children, ...props }) => {
+  return <StyledBlock {...props}>{children}</StyledBlock>;
+};
+
+export default Block;
