705611ed1f5affb797703182f7c6a5133e2c10b7
yuntaengtaeng <k76253218@gmail.com>
2022-04-28 22:16:15 +0900

    feat : Community 카테고리 버튼 컴포넌트 추가

---
+++ src/components/Community/Main/CategoryButton.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PRIMARY } from '../../../constants/color';
+
+const StyledCategoryButton = styled.div`
+  height: 2.625rem;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 1rem;
+  margin-bottom: 0.375rem;
+  display: flex;
+  align-items: center;
+
+  ${({ isSelected }) =>
+    isSelected &&
+    `
+        border-radius: 5px;
+        background-color: #f3f9fe;
+        border: 1px solid #010101;
+        color: ${PRIMARY};
+    `}
+`;
+
+const CategoryButton = ({ onClick, children, isSelected, id }) => {
+  return (
+    <StyledCategoryButton
+      onClick={onClick.bind(this, id)}
+      isSelected={isSelected}
+    >
+      {children}
+    </StyledCategoryButton>
+  );
+};
+
+export default CategoryButton;
