1ac2fa3d4eb5cf3b178899c23129a3a74ae13b98
hdm03 <geg828@nate.com>
2022-05-03 21:16:29 +0900

    feat: ToogleButton 컴포넌트 추가
    - true / false 반환

---
+++ src/components/Ui/ToogleButton.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { PRIMARY, GRAY, WHITE } from '../../constants/color';
+
+const ToogleButtonContainer = styled.div`
+  height: ${(props) => props.size / 2}rem;
+  width: ${(props) => props.size}rem;
+  border-radius: ${(props) => props.size}rem;
+  padding: 0.125rem;
+  background-color: ${(props) => (props.isOpen ? PRIMARY : GRAY)};
+  cursor: pointer;
+`;
+
+const Ball = styled.div`
+  height: ${(props) => props.size / 2}rem;
+  width: ${(props) => props.size / 2}rem;
+  border-radius: 50%;
+  background-color: ${WHITE};
+  transition: 0.5s;
+  transform: ${(props) =>
+    props.isOn ? `translateX(${props.size / 2}rem)` : 'null'};
+`;
+
+const ToogleButton = ({ onClickHandler, isOn, size }) => {
+  const onclick = () => {
+    onClickHandler(!isOn);
+  };
+
+  return (
+    <ToogleButtonContainer onClick={onclick} isOn={isOn} size={size}>
+      <Ball isOn={isOn} size={size}></Ball>
+    </ToogleButtonContainer>
+  );
+};
+
+ToogleButton.propTypes = {
+  size: PropTypes.number,
+  onClickHandler: PropTypes.func,
+  isClicked: PropTypes.bool,
+};
+
+ToogleButton.defaultProps = {
+  size: 2,
+};
+
+export default ToogleButton;
