3c4be44db3a08bc3e8d200ccef3f1f3c79b3bc19
hdm03 <geg828@nate.com>
2022-05-20 21:10:38 +0900

    refactor: 리스트 하나만 보이도록 수정,props 추가
    - 직무 , 기술 리스트만 보여주던 상태에서 리스트를 props로 받아서 보이도록 코드 수정
    - 리스트 정보 받아오는 axios 코드 삭제 props로 대체

--- src/components/Profile/SelectGroub.jsx
+++ src/components/Profile/SelectGroub.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 import SearchSelect from '../Ui/SearchSelect';
 import Chip from '../Ui/Chip';
 
@@ -19,44 +18,16 @@ const ChipsDiv = styled.div`
 `;
 
 const SelectGroub = ({
-  onTechnologieDeletHandler,
-  onPositionDeletHandler,
-  selectedTchnl,
-  selectedJob,
-  onTchnlItemHandler,
-  onJobItemHandler,
+  onDeleteHandler,
+  selectedItems,
+  onSelectItemHandler,
+  options,
   isMe,
+  name,
 }) => {
-  const [positionList, setPositionList] = useState([]);
-  const [technologyList, setTechnologyList] = useState([]);
-
-  useEffect(() => {
-    const renameKeys = (arr) => {
-      const rename = arr.map(({ id, name }) => ({
-        value: id,
-        name,
-      }));
-      return rename;
-    };
-
-    const getList = async () => {
-      try {
-        const [job, technology] = await Promise.all([
-          await axios.get('api/category/job'),
-          await axios.get('api/category/technology'),
-        ]);
-
-        setPositionList([...renameKeys(job.data.jobCategoryList)]);
-        setTechnologyList([...renameKeys(technology.data.technologyList)]);
-      } catch (e) {
-        alert(e.message);
-      }
-    };
-    getList();
-  }, []);
   return (
     <>
-      <StyledP>직무</StyledP>
+      <StyledP>{name}</StyledP>
       {isMe && (
         <SearchSelect
           style={{
@@ -64,51 +35,20 @@ const SelectGroub = ({
             marginTop: '1rem',
             marginBottom: '1rem',
           }}
-          placeholder="직무"
-          options={positionList}
-          onSelectItemHandler={onJobItemHandler}
+          placeholder={name}
+          options={options}
+          onSelectItemHandler={onSelectItemHandler}
         />
       )}
-      {selectedJob.length !== 0 && (
+      {selectedItems.length !== 0 && (
         <ChipsDiv>
-          {selectedJob.map((v) => (
+          {selectedItems.map((v) => (
             <Chip
               key={v.value}
               onDeleteHandler={
                 isMe
                   ? () => {
-                      onPositionDeletHandler(v);
-                    }
-                  : null
-              }
-            >
-              {v.name}
-            </Chip>
-          ))}
-        </ChipsDiv>
-      )}
-      <StyledP>기술</StyledP>
-      {isMe && (
-        <SearchSelect
-          style={{
-            width: '15rem',
-            marginTop: '1rem',
-            marginBottom: '1rem',
-          }}
-          placeholder="기술"
-          options={technologyList}
-          onSelectItemHandler={onTchnlItemHandler}
-        />
-      )}
-      {selectedTchnl.length !== 0 && (
-        <ChipsDiv>
-          {selectedTchnl.map((v) => (
-            <Chip
-              key={v.value}
-              onDeleteHandler={
-                isMe
-                  ? () => {
-                      onTechnologieDeletHandler(v);
+                      onDeleteHandler(v);
                     }
                   : null
               }
