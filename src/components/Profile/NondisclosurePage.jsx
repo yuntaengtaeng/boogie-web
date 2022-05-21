ff68c0cba8c49193588cb0957a9b336b6ab37d9e
yuntaengtaeng <k76253218@gmail.com>
2022-05-21 22:24:49 +0900

    feat : 프로필 비공개 시 비공개 계정이라는 안내를 해주는 컴포넌트 추가

--- src/components/Profile/NondisclosurePage.jsx
+++ src/components/Profile/NondisclosurePage.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import { HiOutlineLockClosed } from 'react-icons/hi';
+
+const Wrap = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h4`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const Description = styled.p`
+  font-size: 1.6rem;
+  font-weight: 200;
+`;
 
 const NondisclosurePage = () => {
-  return <div>NondisclosurePage</div>;
+  return (
+    <Wrap>
+      <Content>
+        <HiOutlineLockClosed size={128} />
+        <Title>비공개 계정입니다.</Title>
+        <Description>
+          프로필 정보를 보려면 프로필을 공개해야 합니다.
+        </Description>
+      </Content>
+    </Wrap>
+  );
 };
 
 export default NondisclosurePage;
