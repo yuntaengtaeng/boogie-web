2dd4b1638284dc8c8c3b2c6ae8af5524bf24cd3d
yuntaengtaeng <k76253218@gmail.com>
2022-06-04 00:07:48 +0900

    add : font Noto Sans 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
 import AppInner from './AppInner';
-import { Reset } from 'styled-reset';
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle` 
+    ${reset}
+    body {
+      font-family: Noto Sans CJK KR
+    }
+`;
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Reset />
+      <GlobalStyles />
       <AppInner></AppInner>
     </Provider>
   );
