0a143842f4184de598b3c5b67bb908d28019c4f5
yuntaengtaeng <k76253218@gmail.com>
2022-09-15 16:18:47 +0900

    fix : useGetCategory response 해당 카테고리의 list 접근 key를 MAP으로 만들어 접근하여 리턴하도록 변경

--- src/hooks/useGetCategory.js
+++ src/hooks/useGetCategory.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const RESPONSE_RESULT_MAP = {
+  job: 'jobCategoryList',
+  region: 'regionList',
+  plattform: 'plattformList',
+  technology: 'technologyList',
+  community: 'communityList',
+  class: 'classList',
+};
+
 const useGetCategory = (code) => {
   const [categorys, setCategorys] = useState([]);
 
@@ -8,7 +17,7 @@ const useGetCategory = (code) => {
     const getCategory = async () => {
       try {
         const response = await axios.get(`api/category/${code}`);
-        setCategorys(response.data);
+        setCategorys(response.data[RESPONSE_RESULT_MAP[code]]);
       } catch (error) {
         if (error.response) {
           alert(error.response.data.message);
@@ -23,6 +32,8 @@ const useGetCategory = (code) => {
       getCategory();
     }
   }, [code]);
+
+  console.log(categorys);
 
   return categorys;
 };
